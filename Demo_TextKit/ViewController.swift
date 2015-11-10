054e98e9009d74cbfe2e491f6d3c7164ab155b52
Yu Pengyang <yupengyang@caishuo.com>
2015-11-10 11:41:20 +0800

    init demo project

---
+++ Demo_TextKit/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo_TextKit
+//
+//  Created by Yu Pengyang on 11/10/15.
+//  Copyright (c) 2015 Yu Pengyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
