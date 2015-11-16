a24eede2129b539d7b0adfa30738ad472ebb9782
Yu Pengyang <yupengyang@caishuo.com>
2015-11-16 15:01:34 +0800

    add fade animation

--- Demo_TextKit/TextAnimationViewController.swift
+++ Demo_TextKit/TextAnimationViewController.swift
@@ -35,10 +35,24 @@ class TextAnimationViewController: ViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     @IBAction func tapButton(sender: AnyObject) {
+        for var i = 0; i < layers.count; i++ {
+            let l = layers[i]
+            let duration = (Double(arc4random() % 100) / 200.0) + 0.5
+            let delay = Double(arc4random() % 100) / 100.0
+            println(delay)
+            dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Double(NSEC_PER_SEC) * delay)), dispatch_get_main_queue(), { () -> Void in
+                CATransaction.begin()
+                CATransaction.setDisableActions(true)
+                CATransaction.setAnimationDuration(duration)
+                CATransaction.setAnimationTimingFunction(CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut))
+                l.opacity = 1
+                CATransaction.commit()
+            })
+        }
+        
     }
     
     private func setup() {
@@ -49,18 +63,32 @@ class TextAnimationViewController: ViewController {
     }
 
     private func setupString(string: String) {
-        let attri = NSAttributedString(string: string, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(16)])
+        let attri = NSAttributedString(string: string, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(16), NSForegroundColorAttributeName: UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)])
         textStorage.setAttributedString(attri)
     }
     
     private func display() {
         let range = layoutManager.glyphRangeForTextContainer(textContainer)
-        let rect = layoutManager.usedRectForTextContainer(textContainer)
-        println("textContainer rect: \(rect)")
+        let usedRect = layoutManager.usedRectForTextContainer(textContainer)
+        let origin = CGPoint(x: 20, y: 200)
+        println("textContainer rect: \(usedRect)")
         for var i = range.location; i < NSMaxRange(range); {
             var r = NSMakeRange(i, 1)
             let charRange = layoutManager.characterRangeForGlyphRange(r, actualGlyphRange: &r)
             let glyphRect = layoutManager.boundingRectForGlyphRange(r, inTextContainer: textContainer)
+            
+            let textForLayer = textStorage.attributedSubstringFromRange(charRange)
+            var layerFrame = CGRectMake(origin.x + glyphRect.origin.x - glyphRect.width, origin.y + glyphRect.origin.y, glyphRect.width * 3, glyphRect.height)
+            
+            let textLayer = CATextLayer()
+            textLayer.contentsScale = UIScreen.mainScreen().scale
+            textLayer.string = textForLayer//.string
+            textLayer.alignmentMode = kCAAlignmentCenter
+            textLayer.frame = layerFrame
+            textLayer.opacity = 0
+            view.layer.addSublayer(textLayer)
+            layers.append(textLayer)
+            i += r.length
         }
     }
 }
