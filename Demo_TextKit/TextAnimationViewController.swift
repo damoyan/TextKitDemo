462843d856ca3213fb2bb0975e2ce86d38c26db1
Yu Pengyang <yupengyang@caishuo.com>
2015-11-16 19:19:48 +0800

    refine code

--- Demo_TextKit/TextAnimationViewController.swift
+++ Demo_TextKit/TextAnimationViewController.swift
@@ -15,7 +15,7 @@ class TextAnimationViewController: ViewController {
     let textStorage = NSTextStorage()
     let layoutManager = NSLayoutManager()
     let textContainer = NSTextContainer()
-    var layers = [CALayer]()
+    var layers = [CATextLayer]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,12 +24,12 @@ class TextAnimationViewController: ViewController {
         setup()
         setupString("just test for the effect")
 //        setupString(displayString)
-        label.attributedText = textStorage
+//        label.attributedText = textStorage
+//        print(label.font)
     }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
-        print("label: \(label.bounds.size)")
         textContainer.size = CGSize(width: label.bounds.size.width, height: CGFloat.max)
     }
 
@@ -42,6 +42,7 @@ class TextAnimationViewController: ViewController {
             let l = layers[i]
             let duration = (Double(arc4random() % 100) / 200.0) + 0.5
             let delay = Double(arc4random() % 100) / 100.0
+            l.opacity = 0
             dispatch_after(dispatch_time(DISPATCH_TIME_NOW, Int64(Double(NSEC_PER_SEC) * delay)), dispatch_get_main_queue(), { () -> Void in
                 CATransaction.begin()
                 CATransaction.setDisableActions(true)
@@ -62,28 +63,39 @@ class TextAnimationViewController: ViewController {
     }
 
     private func setupString(string: String) {
-        let attri = NSAttributedString(string: string, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(16), NSForegroundColorAttributeName: UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)])
+        //NSFontAttributeName: UIFont(name: "Papyrus", size: 16)!,
+        let attri = NSAttributedString(string: string, attributes: [NSFontAttributeName: UIFont(name: "Papyrus", size: 16)!, NSForegroundColorAttributeName: UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1)])
         textStorage.setAttributedString(attri)
     }
     
     private func display() {
+        layers.removeAll(keepCapacity: false)
         let range = layoutManager.glyphRangeForTextContainer(textContainer)
-        let usedRect = layoutManager.usedRectForTextContainer(textContainer)
         let origin = CGPoint(x: 20, y: 200)
-        print("textContainer rect: \(usedRect)")
         for var i = range.location; i < NSMaxRange(range); {
             var r = NSMakeRange(i, 1)
             let charRange = layoutManager.characterRangeForGlyphRange(r, actualGlyphRange: &r)
             let glyphRect = layoutManager.boundingRectForGlyphRange(r, inTextContainer: textContainer)
             
             let textForLayer = textStorage.attributedSubstringFromRange(charRange)
-            let layerFrame = CGRectMake(origin.x + glyphRect.origin.x - glyphRect.width
-                , origin.y + glyphRect.origin.y, glyphRect.width * 3
-                , glyphRect.height)
+            var layerFrame: CGRect
+            if layers.count == 0 {
+                layerFrame = CGRectMake(origin.x + glyphRect.origin.x, origin.y + glyphRect.origin.y, glyphRect.width, glyphRect.height)
+            } else {
+                layerFrame = CGRectMake(origin.x + glyphRect.origin.x - glyphRect.width, origin.y + glyphRect.origin.y, glyphRect.width * 3, glyphRect.height)
+            }
             
             let textLayer = CATextLayer()
-            textLayer.string = textForLayer//.string
+            /// if set `string` with NSAttributedString, we still need to set the `font` and `fontSize` property
+            /// or the characters displayed will be a little bolder than original font.
+            /// note: when set NSAttributedString without font attribute to NSTextStorage, it will automatically add NSFontAttribute with default font.
+            textLayer.string = textForLayer
+            if let font = textForLayer.attribute(NSFontAttributeName, atIndex: 0, effectiveRange: nil) as? UIFont {
+                textLayer.font = CTFontCreateWithName(font.fontName, font.pointSize, nil)
+                textLayer.fontSize = font.pointSize
+            }
             textLayer.alignmentMode = kCAAlignmentCenter
+            textLayer.foregroundColor = UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 1).CGColor
             textLayer.frame = layerFrame
             textLayer.opacity = 0
             textLayer.wrapped = true
