767998a4ef161628fd3e57ac3a63d13842449fa4
Yu Pengyang <yupengyang@caishuo.com>
2015-11-16 10:12:50 +0800

    add animation part

--- Demo_TextKit/AppDelegate.swift
+++ Demo_TextKit/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        setupUI()
         return true
     }
 
@@ -41,6 +42,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-
+    private func setupUI() {
+        UINavigationBar.appearance().backgroundColor = UIColor.whiteColor()
+    }
 }
 
--- Demo_TextKit/TextAnimationViewController.swift
+++ Demo_TextKit/TextAnimationViewController.swift
@@ -10,10 +10,27 @@ import UIKit
 
 class TextAnimationViewController: ViewController {
 
+    @IBOutlet weak var label: UILabel!
+    
+    let textStorage = NSTextStorage()
+    let layoutManager = NSLayoutManager()
+    let textContainer = NSTextContainer()
+    var layers = [CALayer]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        setup()
+        setupString("just test for the effect")
+//        setupString(displayString)
+        label.attributedText = textStorage
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        println("label: \(label.bounds.size)")
+        textContainer.size = CGSize(width: label.bounds.size.width, height: CGFloat.max)
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +38,39 @@ class TextAnimationViewController: ViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func tapButton(sender: AnyObject) {
     }
-    */
+    
+    private func setup() {
+        textContainer.lineFragmentPadding = 0
+        textStorage.addLayoutManager(layoutManager)
+        layoutManager.addTextContainer(textContainer)
+        layoutManager.delegate = self
+    }
 
+    private func setupString(string: String) {
+        let attri = NSAttributedString(string: string, attributes: [NSFontAttributeName: UIFont.systemFontOfSize(16)])
+        textStorage.setAttributedString(attri)
+    }
+    
+    private func display() {
+        let range = layoutManager.glyphRangeForTextContainer(textContainer)
+        let rect = layoutManager.usedRectForTextContainer(textContainer)
+        println("textContainer rect: \(rect)")
+        for var i = range.location; i < NSMaxRange(range); {
+            var r = NSMakeRange(i, 1)
+            let charRange = layoutManager.characterRangeForGlyphRange(r, actualGlyphRange: &r)
+            let glyphRect = layoutManager.boundingRectForGlyphRange(r, inTextContainer: textContainer)
+        }
+    }
+}
+
+extension TextAnimationViewController: NSLayoutManagerDelegate {
+    func layoutManager(layoutManager: NSLayoutManager, didCompleteLayoutForTextContainer textContainer: NSTextContainer?, atEnd layoutFinishedFlag: Bool) {
+        display()
+    }
+    
+//    func layoutManager(layoutManager: NSLayoutManager, textContainer: NSTextContainer, didChangeGeometryFromSize oldSize: CGSize) {
+//        display()
+//    }
 }
